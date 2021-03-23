325d499115a9e9b9c45ec9a41e54f83900aa3889
Fernando Saeta <fernando.r.saeta@gmail.com>
2021-03-23 11:44:49 -0300

    [IMP] Adiciona módulo para adicionar formio nos projetos

---
+++ formio_project/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    'name': 'Forms | Project',
+    'summary': 'Forms integration with Projects',
+    'version': '12.0.1.0.0',
+    'license': 'LGPL-3',
+    'author': 'MultidadosTI',
+    'website': 'www.multidadosti.com.br',
+    'category': 'Project',
+    'contributors': [
+        'Fernando Saeta <fernando.r.saeta@gmail.com>',
+    ],
+    'depends': ['project',
+                'formio',
+                'formio_data_api'
+    ],
+    'data': [
+        'data/formio_project_data.xml',
+        'views/formio_form_views.xml',
+        'views/project_views.xml',
+    ],
+    'application': True,
+    'installable': True,
+    'images': [
+        'static/description/banner.gif',
+    ],
+    'description': """
+Forms | Project
+=============
+
+"""
+}
